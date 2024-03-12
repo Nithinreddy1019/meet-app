e1ae2755aa6303cd9d262b4ef343098dae34f0aa
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-03-12 20:12:13 +0530

    new logs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import {Routes, Route} from 'react-router-dom'
-import Signup from './pages/Signup'
-import Home from './pages/Home'
-import Signin from './pages/Signin'
-
 function App() {
   const [darkMode, setDarkMode] = useState(false)
 
@@ -14,16 +9,7 @@ function App() {
 
   return (
     <div className={`${darkMode ? "dark" : null}`}>
-      <div className='absolute bottom-[1%] right-0 px-2 py-2 border'>
-        <button onClick={darkModeHandler}>
-          Dark
-        </button>
-      </div>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/signup' element={<Signup />}/>
-        <Route path='/signin' element={<Signin />}/>
-      </Routes>
+      
     </div>
   )
 }
