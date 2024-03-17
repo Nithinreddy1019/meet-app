f944be86b4c4e9735b9ecc841fde7f7d077973ed
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-03-17 13:42:24 +0530

    Added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import NavBar from './components/Navbar'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
 
 
 
 function App() {
   return (
-    <div>
-      <NavBar />
-    </div>
+    <Routes>
+      <Route path={'/'} element={<Home />}/>
+      <Route path={'/signup'} element={<Signup />}/>
+      <Route path={'/signin'} element={<Signin />} />
+    </Routes>
   )
 }
 
