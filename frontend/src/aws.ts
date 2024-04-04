49569fad3fc82d41e54e07e1c513ac0befe3d254
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-04-04 17:54:50 +0530

    aws

--- frontend/src/aws.ts
+++ frontend/src/aws.ts
@@ -6,7 +6,9 @@ export const uploadImage = async (img: File) => {
         return("No image selected")
     };
 
-    const res = await axios.post(`${BACKEND}/api/v1/img/putpresignedurl`, {filename: img.name, type: img.type})
+    const res = await axios.post(`${BACKEND}/api/v1/img/putpresignedurl`, {filename: img.name, type: img.type}, {headers: {
+        "Authorization": `Bearer ${localStorage.getItem('token')}`
+    }})
     const  uploadUrl = res.data.msg;
 
     await axios.put(uploadUrl, img, {headers: {
